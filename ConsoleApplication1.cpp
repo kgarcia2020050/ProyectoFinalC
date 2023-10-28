c8f9346213c0755908b141017cf15de7b0d3e349
SamuelEscobarV <sescobar@nexabanco.com>
2023-10-27 19:09:04 -0600

    feat: :sparkles: (Diseño) Se cambio el diseño y los delimitadores por comas.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <string>
 #include <locale>
+#include <iomanip>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -17,15 +20,159 @@ struct Empleado
     string status = "activo";
 };
 
-int obtenerUltimoId()
+int maxIdWidth = 4;
+int maxNombreWidth = 8;
+int maxApellidoWidth = 9;
+int maxFechaWidth = 17;
+int maxDPIWidth = 3;
+int maxDireccionWidth = 9;
+int maxTelefonoWidth = 8;
+
+void calcularAnchoCampos()
 {
-    int ultimoId = 0;
     ifstream file("empleados.txt");
     if (file)
     {
         Empleado emp;
-        while (file >> emp.id >> emp.nombres >> emp.apellidos >> emp.fechaNacimiento >> emp.DPI >> emp.direccion >> emp.telefono)
+        string line;
+
+        while (getline(file, line))
         {
+            stringstream ss(line);
+            string token;
+
+            getline(ss, token, ','); 
+            emp.id = stoi(token);
+
+            getline(ss, emp.nombres, ',');
+            getline(ss, emp.apellidos, ',');
+            getline(ss, emp.fechaNacimiento, ',');
+            getline(ss, emp.DPI, ',');
+            getline(ss, emp.direccion, ',');
+            getline(ss, emp.telefono, ',');
+            getline(ss, emp.status, ',');
+
+            if (emp.status == "activo")
+            {
+                maxIdWidth = max(maxIdWidth, (int)to_string(emp.id).size());
+                maxNombreWidth = max(maxNombreWidth, (int)emp.nombres.size());
+                maxApellidoWidth = max(maxApellidoWidth, (int)emp.apellidos.size());
+                maxFechaWidth = max(maxFechaWidth, (int)emp.fechaNacimiento.size());
+                maxDPIWidth = max(maxDPIWidth, (int)emp.DPI.size());
+                maxDireccionWidth = max(maxDireccionWidth, (int)emp.direccion.size());
+                maxTelefonoWidth = max(maxTelefonoWidth, (int)emp.telefono.size());
+            }
+        }
+        file.close();
+    }
+}
+
+void imprimirLineaHorizontal()
+{
+    cout << "+";
+    cout << setw(maxIdWidth + 2) << setfill('-') << "";
+    cout << "+";
+    cout << setw(maxNombreWidth + 2) << "";
+    cout << "+";
+    cout << setw(maxApellidoWidth + 2) << "";
+    cout << "+";
+    cout << setw(maxFechaWidth + 2) << "";
+    cout << "+";
+    cout << setw(maxDPIWidth + 2) << "";
+    cout << "+";
+    cout << setw(maxDireccionWidth + 2) << "";
+    cout << "+";
+    cout << setw(maxTelefonoWidth + 2) << "";
+    cout << "+";
+    cout << endl;
+    cout << setfill(' ');
+}
+
+void mostrarTablaEmpleados(const vector<Empleado> &empleados)
+{
+    calcularAnchoCampos();
+
+    if (empleados.empty())
+    {
+        cout << "No hay empleados para mostrar." << endl;
+        return;
+    }
+
+    imprimirLineaHorizontal();
+    cout << "+ " << setw(maxIdWidth) << left << "ID"
+         << " + " << setw(maxNombreWidth) << "Nombres"
+         << " + " << setw(maxApellidoWidth) << "Apellidos"
+         << " + " << setw(maxFechaWidth) << "Fecha Nacimiento"
+         << " + " << setw(maxDPIWidth) << "DPI"
+         << " + " << setw(maxDireccionWidth) << "Dirección"
+         << " + " << setw(maxTelefonoWidth) << "Teléfono"
+         << " +" << endl;
+    imprimirLineaHorizontal();
+
+    for (const Empleado &emp : empleados)
+    {
+        cout << "| " << setw(maxIdWidth) << left << emp.id
+             << " | " << setw(maxNombreWidth) << emp.nombres
+             << " | " << setw(maxApellidoWidth) << emp.apellidos
+             << " | " << setw(maxFechaWidth) << emp.fechaNacimiento
+             << " | " << setw(maxDPIWidth) << emp.DPI
+             << " | " << setw(maxDireccionWidth) << emp.direccion
+             << " | " << setw(maxTelefonoWidth) << emp.telefono
+             << " |" << endl;
+    }
+    imprimirLineaHorizontal();
+}
+
+void imprimirEmpleado(const Empleado &emp)
+{
+    imprimirLineaHorizontal();
+    cout << "| " << setw(maxIdWidth) << left << "ID"
+         << " | " << setw(maxNombreWidth) << "Nombres"
+         << " | " << setw(maxApellidoWidth) << "Apellidos"
+         << " | " << setw(maxFechaWidth) << "Fecha Nacimiento"
+         << " | " << setw(maxDPIWidth) << "DPI"
+         << " | " << setw(maxDireccionWidth) << "Dirección"
+         << " | " << setw(maxTelefonoWidth) << "Teléfono"
+         << " |" << endl;
+    imprimirLineaHorizontal();
+    cout << "| " << setw(maxIdWidth) << left << emp.id << " | "
+         << setw(maxNombreWidth) << emp.nombres << " | "
+         << setw(maxApellidoWidth) << emp.apellidos << " | "
+         << setw(maxFechaWidth) << emp.fechaNacimiento << " | "
+         << setw(maxDPIWidth) << emp.DPI << " | "
+         << setw(maxDireccionWidth) << emp.direccion << " | "
+         << setw(maxTelefonoWidth) << emp.telefono << " |" << endl;
+    imprimirLineaHorizontal();
+}
+
+void lineasDecorativas(int longitud)
+{
+    cout << "+";
+    for (int i = 0; i < longitud; i++)
+    {
+        cout << "-";
+    }
+    cout << "+" << endl;
+}
+
+int obtenerUltimoId()
+{
+    int ultimoId = 0;
+    ifstream file("empleados.txt");
+    string token;
+    if (file)
+    { 
+        Empleado emp;
+        while (getline(file, token, ','))
+        {
+            emp.id = stoi(token); // Convertimos el string a int
+            getline(file, emp.nombres, ',');
+            getline(file, emp.apellidos, ',');
+            getline(file, emp.fechaNacimiento, ',');
+            getline(file, emp.DPI, ',');
+            getline(file, emp.direccion, ',');
+            getline(file, emp.telefono, ',');
+            getline(file, emp.status);
             ultimoId = emp.id;
         }
         file.close();
@@ -35,33 +182,63 @@ int obtenerUltimoId()
 
 void guardarEmpleado(const Empleado &emp)
 {
+    lineasDecorativas(30);
     ofstream file("empleados.txt", ios::app);
     if (file)
     {
-        file << emp.id << " " << emp.nombres << " " << emp.apellidos << " " << emp.fechaNacimiento << " " << emp.DPI << " " << emp.direccion << " " << emp.telefono << " " << emp.status << endl;
+        file << emp.id << "," << emp.nombres << "," << emp.apellidos << "," << emp.fechaNacimiento << "," << emp.DPI << "," << emp.direccion << "," << emp.telefono << "," << emp.status << endl;
         file.close();
+        lineasDecorativas(30);
+        cout << "Empleado agregado con éxito." << endl;
+        lineasDecorativas(30);
+
+        calcularAnchoCampos();
+
+        cout << "\nDatos del empleado guardado:\n";
+        imprimirEmpleado(emp);
     }
-    cout << "Empleado agregado con éxito." << endl;
+    else
+    {
+        cout << "Error al guardar el empleado." << endl;
+    }
+    lineasDecorativas(30);
 }
 
 void mostrarEmpleados()
 {
+    vector<Empleado> empleadosActivos;
+
     ifstream file("empleados.txt");
     if (file)
     {
         Empleado emp;
-        while (file >> emp.id >> emp.nombres >> emp.apellidos >> emp.fechaNacimiento >> emp.DPI >> emp.direccion >> emp.telefono >> emp.status)
+        string line;
+
+        while (getline(file, line))
         {
-            if (emp.status == "activo") {
-                cout << "ID: " << emp.id << "\nNombres: " << emp.nombres << "\nApellidos: " << emp.apellidos << "\nFecha de Nacimiento: " << emp.fechaNacimiento << "\nDPI: " << emp.DPI << "\nDirección: " << emp.direccion << "\nTeléfono: " << emp.telefono << "\n" << endl;
+            stringstream ss(line);
+            string token;
+
+            getline(ss, token, ','); 
+            emp.id = stoi(token);
+
+            getline(ss, emp.nombres, ',');
+            getline(ss, emp.apellidos, ',');
+            getline(ss, emp.fechaNacimiento, ',');
+            getline(ss, emp.DPI, ',');
+            getline(ss, emp.direccion, ',');
+            getline(ss, emp.telefono, ',');
+            getline(ss, emp.status, ',');
+
+            if (emp.status == "activo")
+            {
+                empleadosActivos.push_back(emp);
             }
         }
         file.close();
     }
-    else
-    {
-        cout << "No hay empleados registrados." << endl;
-    }
+
+    mostrarTablaEmpleados(empleadosActivos);
 }
 
 bool contiene(const string &cadena, const string &termino)
@@ -86,80 +263,136 @@ void buscarEmpleados()
     ifstream file("empleados.txt");
     string termino;
 
-    cout << "Ingrese el término de búsqueda (Apellidos, ID empleado,etc.) ";
-    cin >> termino;
+    lineasDecorativas(30);
+    cout << "Ingrese el término de búsqueda (Apellidos, ID empleado,etc.): ";
+    cin.ignore();
+    getline(cin, termino);
+    lineasDecorativas(30);
+    vector<Empleado> empleadosEncontrados;
 
     if (file)
     {
         Empleado emp;
-        bool encontrado = false;
+        string line;
 
-        while (file >> emp.id >> emp.nombres >> emp.apellidos >> emp.fechaNacimiento >> emp.DPI >> emp.direccion >> emp.telefono >> emp.status)
+        while (getline(file, line))
         {
-            if (emp.status == "activo") {
-                if (contiene(to_string(emp.id), termino) ||
-                    contiene(emp.nombres, termino) ||
-                    contiene(emp.apellidos, termino) ||
-                    contiene(emp.fechaNacimiento, termino) ||
-                    contiene(emp.DPI, termino) ||
-                    contiene(emp.direccion, termino) ||
-                    contiene(emp.telefono, termino))
-                {
+            stringstream ss(line);
+            string token;
 
-                    cout << "ID: " << emp.id << "\nNombres: " << emp.nombres << "\nApellidos: " << emp.apellidos << "\nFecha de Nacimiento: " << emp.fechaNacimiento << "\nDPI: " << emp.DPI << "\nDirección: " << emp.direccion << "\nTeléfono: " << emp.telefono << "\n" << endl;
-                    encontrado = true;
-                }   
+            getline(ss, token, ','); 
+            emp.id = stoi(token);
+            
+            getline(ss, emp.nombres, ',');
+            getline(ss, emp.apellidos, ',');
+            getline(ss, emp.fechaNacimiento, ',');
+            getline(ss, emp.DPI, ',');
+            getline(ss, emp.direccion, ',');
+            getline(ss, emp.telefono, ',');
+            getline(ss, emp.status, ',');
+            
+            if (emp.status == "activo" && (
+                contiene(to_string(emp.id), termino) ||
+                contiene(emp.nombres, termino) ||
+                contiene(emp.apellidos, termino) ||
+                contiene(emp.fechaNacimiento, termino) ||
+                contiene(emp.DPI, termino) ||
+                contiene(emp.direccion, termino) ||
+                contiene(emp.telefono, termino))
+            )
+            {
+                empleadosEncontrados.push_back(emp);
             }
-        }
-
-        if (!encontrado)
-        {
-            cout << "No se encontró ningún empleado con el término: " << termino << endl;
         }
 
         file.close();
     }
-    else
-    {
-        cout << "No hay empleados registrados." << endl;
-    }
+
+    mostrarTablaEmpleados(empleadosEncontrados);
 }
 
-void deleteEmpleado() {
-    ifstream file("empleados.txt");
-    ofstream temp("temp.txt");
+void eliminarEmpleado()
+{
+    Empleado emp;
     int idEliminar;
     bool encontrado = false;
 
+    fstream file("empleados.txt", ios::in);
+    if (!file)
+    {
+        cerr << "No se pudo abrir el archivo." << endl;
+        return;
+    }
+
+    ofstream temp("temp.txt", ios::out);
+    if (!temp)
+    {
+        cerr << "No se pudo crear el archivo temporal." << endl;
+        return;
+    }
+
+    lineasDecorativas(30);
     cout << "Ingrese el ID del empleado a eliminar: ";
     cin >> idEliminar;
+    cin.ignore();
+    lineasDecorativas(30);
 
-    if (file && temp) {
-        Empleado emp;
-        while (file >> emp.id >> emp.nombres >> emp.apellidos >> emp.fechaNacimiento >> emp.DPI >> emp.direccion >> emp.telefono >> emp.status) {
-            if (emp.id == idEliminar && emp.status == "activo") {
-                emp.status = "eliminado";
-                encontrado = true;
+    string line;
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+
+        getline(ss, line, ','); 
+        emp.id = stoi(line);
+        
+        getline(ss, emp.nombres, ',');
+        getline(ss, emp.apellidos, ',');
+        getline(ss, emp.fechaNacimiento, ',');
+        getline(ss, emp.DPI, ',');
+        getline(ss, emp.direccion, ',');
+        getline(ss, emp.telefono, ',');
+        getline(ss, emp.status, ',');
+
+        if (emp.id == idEliminar && emp.status == "activo")
+        {
+            emp.status = "eliminado";
+            encontrado = true;
+            char confirmacion;
+            lineasDecorativas(30);
+            cout << "¿Está seguro de que desea eliminar este registro? (s/n): \n";
+            lineasDecorativas(30);
+            imprimirEmpleado(emp);
+            lineasDecorativas(30);
+            cin >> confirmacion;
+            if (tolower(confirmacion) == 's')
+            {
+                lineasDecorativas(30);
                 cout << "Empleado con ID " << idEliminar << " ha sido eliminado." << endl;
             }
-            temp << emp.id << " " << emp.nombres << " " << emp.apellidos << " " << emp.fechaNacimiento << " " << emp.DPI << " " << emp.direccion << " " << emp.telefono << " " << emp.status << endl;
+            else
+            {
+                emp.status = "activo";
+                cout << "Eliminación cancelada." << endl;
+            }
         }
-        if (!encontrado) {
-            cout << "No se encontró ningún empleado con el ID: " << idEliminar << endl;
-        }
-
-        file.close();
-        temp.close();
-
-        // Reemplaza el archivo original con el temporal
-        remove("empleados.txt");
-        rename("temp.txt", "empleados.txt");
+        temp << emp.id << "," << emp.nombres << "," << emp.apellidos
+             << "," << emp.fechaNacimiento << "," << emp.DPI
+             << "," << emp.direccion << "," << emp.telefono
+             << "," << emp.status << endl;
     }
-    else {
-        cout << "Error al acceder al archivo de empleados." << endl;
+
+    if (!encontrado) {
+        cout << "No se encontró un empleado con el ID " << idEliminar << "." << endl;
     }
+    lineasDecorativas(30);
+
+    file.close();
+    temp.close();
+    
+    remove("empleados.txt");
+    rename("temp.txt", "empleados.txt");
 }
-
 
 void main()
 {
@@ -167,14 +400,22 @@ void main()
     int option;
     int ultimoId = obtenerUltimoId();
 
+    lineasDecorativas(30);
+    cout << "Exitos.com" << endl;
+    lineasDecorativas(30);
+    cout << "Bienvenido" << endl;
+
     do
     {
+        lineasDecorativas(30);
         cout << "Menú Principal" << endl;
+        lineasDecorativas(30);
         cout << "1. Ingreso de datos de empleados" << endl;
         cout << "2. Mostrar lista de empleados" << endl;
         cout << "3. Buscar un empleado" << endl;
         cout << "4. Eliminar registro de empleado" << endl;
         cout << "5. Salir" << endl;
+        lineasDecorativas(30);
         cout << "Ingrese la opción: ";
         cin >> option;
 
@@ -213,15 +454,19 @@ void main()
             break;
 
         case 4:
-            deleteEmpleado();
+            eliminarEmpleado();
             break;
 
         case 5:
+            lineasDecorativas(30);
             cout << "Saliendo del programa." << endl;
+            lineasDecorativas(30);
             break;
 
         default:
+            lineasDecorativas(30);
             cout << "Opción inválida. Intente de nuevo." << endl;
+            lineasDecorativas(30);
         }
     } while (option != 5);
 }
